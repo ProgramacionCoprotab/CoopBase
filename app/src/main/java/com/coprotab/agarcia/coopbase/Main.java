8b51f485702f51146c04b7c308e3019aef2f54a4
ProgramacionCoprotab <cla19pwd>
2016-10-04 08:35:50 -0300

    okok

--- app/src/main/java/com/coprotab/agarcia/coopbase/Main.java
+++ app/src/main/java/com/coprotab/agarcia/coopbase/Main.java
@@ -4,6 +4,10 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -12,6 +16,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main extends ActionBarActivity {
     String codrack = "";
@@ -29,9 +34,19 @@ public class Main extends ActionBarActivity {
         txtCodRack.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (keyCode == KeyEvent.KEYCODE_ENTER) {;
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    validarFormatoRack(txtCodRack.getText().toString());
                     codrack = txtCodRack.getText().toString();
-                    Alert("Presionó ENTER","Recibido",Main.this);
+                    //validar si es rack
+                    if(OnLine())
+                    {
+                        //Trabaja online
+                    }
+                    else
+                    {
+                        //Ingresa en tablas auxiliares
+                    }
+
                     return true;
                 } else {
                     return false;
@@ -48,7 +63,8 @@ public class Main extends ActionBarActivity {
               .setPositiveButton("ok", new DialogInterface.OnClickListener() {
                   @Override
                   public void onClick(DialogInterface dialog, int which) {
-                      // Whatever...
+                      // código al dar click en OK
+
                   }
               }).create().show();
     }
@@ -60,7 +76,77 @@ public class Main extends ActionBarActivity {
         return true;
     }
 
+    protected boolean validarFormatoRack(String rack)
+    {
+        try {
+            String inicial = rack.substring(0, 1);
+            Integer longitud = rack.length();
+            if (inicial.equals( "0" )) {
+
+                if (rack.length() == 8) return true;
+
+            else
+                return false;
+            }
+            else
+
+                return false;
+
+        }
+        catch (Exception ex){
+            ex.getMessage();
+            return false;
+        }
+
+    }
+
+    protected boolean validarFormatoFardo(String fardo){
+        try
+        {
+            Integer longitud = fardo.length();
+            if(longitud == 9)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+        catch (Exception ex){
+            return false;
+        }
+    }
+
+
     protected boolean validarFardoOnLine(){
         return true;
+    }
+
+
+    //procedimiento para conocer la red a la que esta conectado el dispositivo
+    protected boolean OnLine(){
+        try {
+            boolean connected = false;
+            WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo;
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+                wifiInfo = wifi.getConnectionInfo();
+                String ssidWifi = wifiInfo.getSSID().substring(1,11);
+                if (ssidWifi.equals("APCoprotab")) {
+                    return connected = true;
+                } else {
+                    return connected = false;
+                }
+            } else
+                return connected = false;
+        }
+        catch(Exception ex){
+            ex.getMessage();
+            return false;
+        }
     }
 }
